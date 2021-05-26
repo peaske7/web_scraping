65c30122adbdaa1f3a70de8ca1ce00378c8a1163
DrPoppyseed <peaske16180@gmail.com>
2021-05-26 13:04:13 +0900

    genspider with crawl template

--- imdb/imdb/spiders/best_movies.py
+++ imdb/imdb/spiders/best_movies.py
@@ -6,11 +6,25 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc']
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+
+    def start_requests(self):
+      yield scrapy.Request(url="https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc", headers={
+        'User-Agent': self.user_agent
+      })
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths='//h3[@class="lister-item-header"]/a'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//h3[@class="lister-item-header"]/a'), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths='(//a[@class="lister-page-next next-page"])[2]'), process_request='set_user_agent')
     )
 
+    def set_user_agent(self, request, spider):
+      request.headers['User-Agent'] = self.user_agent
+      return request
+
     def parse_item(self, response):
-        print(response.url)
+        yield {
+          'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+          'durationg': response.xpath("normalize-space((//time)[1]/text())").get(),
+          'movie_url': response.url
+        }
